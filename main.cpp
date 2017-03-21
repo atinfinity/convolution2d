463f35ac5b3b2ceb5cee8638e51268af306759d0
atinfinity <dandelion1124@gmail.com>
2017-03-22 01:45:44 +0900

    added verify code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 ﻿#include "convolution2d.hpp"
+#include "convolution2d.cuh"
+#include "utility.hpp"
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
@@ -12,6 +14,7 @@ int main(int argc, const char* argv[])
 	}
 
 	cv::Mat dst(src.size(), src.type(), cv::Scalar(0));
+	cv::Mat dstGpu(src.size(), src.type(), cv::Scalar(0));
 
 	// kernel
 	const int kernel_size = 3;
@@ -23,16 +26,28 @@ int main(int argc, const char* argv[])
 			1.0f / 9, 1.0f / 9, 1.0f / 9
 		);
 
+	cv::cuda::GpuMat d_src(src);
+	cv::cuda::GpuMat d_dstGpu(src.size(), src.type(), cv::Scalar(0));
+	cv::cuda::GpuMat d_kernel(kernel);
+
 	const int loop_num = 10;
 
 	// convolution
-	double time = launch_convolution2d(src, dst, kernel, loop_num);
+	double time_cpu = launch_convolution2d(src, dst, kernel, loop_num);
+	double time_gpu = launch_convolution2dGpu(d_src, d_dstGpu, d_kernel, loop_num);
 
-	std::cout << "time = " << time << " ms." << std::endl;
+	std::cout << "time(cpu) = " << time_cpu << " ms." << std::endl;
+	std::cout << "time(gpu) = " << time_gpu << " ms." << std::endl;
+
+	// verify
+	verify(dst, d_dstGpu);
 
 	// display image
 	cv::imshow("src", src);
 	cv::imshow("dst", dst);
+	d_dstGpu.download(dstGpu);
+	cv::imshow("dst(gpu)", dstGpu);
+
 	cv::waitKey(0);
 	cv::destroyAllWindows();
 
