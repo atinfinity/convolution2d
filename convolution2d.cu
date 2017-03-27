db26d22683359967c6d7ab371c1079716d79ccd9
atinfinity <dandelion1124@gmail.com>
2017-03-27 23:26:25 +0900

    refactoring

--- convolution2d.cu
+++ convolution2d.cu
@@ -7,67 +7,67 @@
 
 __device__ uchar clipGpu(float val)
 {
-    return (val < 0.0f) ? 0 : (val > 255.0f) ? 255 : (uchar)val;
+	return (val < 0.0f) ? 0 : (val > 255.0f) ? 255 : (uchar)val;
 }
 
 __global__ void convolution2dGpu
 (
-    const cv::cudev::PtrStepSz<uchar> src,
-    cv::cudev::PtrStepSz<uchar> dst,
-    const cv::cudev::PtrStepSz<float> kernel
+	const cv::cudev::PtrStepSz<uchar> src,
+	cv::cudev::PtrStepSz<uchar> dst,
+	const cv::cudev::PtrStepSz<float> kernel
 )
 {
-    const int x = blockDim.x * blockIdx.x + threadIdx.x;
-    const int y = blockDim.y * blockIdx.y + threadIdx.y;
-    const int half_size = (kernel.cols / 2);
+	const int x = blockDim.x * blockIdx.x + threadIdx.x;
+	const int y = blockDim.y * blockIdx.y + threadIdx.y;
+	const int half_size = (kernel.cols / 2);
 
-    if((y >= half_size) && y < (src.rows - half_size)){
-        if((x >= half_size) && (x < (src.cols - half_size))){
-            float sum = 0.0f;
-            for (int dy = -half_size; dy <= half_size; dy++){
-                for (int dx = -half_size; dx <= half_size; dx++){
-                    sum = __fadd_rn(sum, __fmul_rn(kernel.ptr(dy+half_size)[dx+half_size], src.ptr(y+dy)[x+dx]));
-                }
-            }
-            dst.ptr(y)[x] = clipGpu(sum);
-        }
-    }
+	if((y >= half_size) && y < (src.rows - half_size)){
+		if((x >= half_size) && (x < (src.cols - half_size))){
+			float sum = 0.0f;
+			for (int dy = -half_size; dy <= half_size; dy++){
+				for (int dx = -half_size; dx <= half_size; dx++){
+					sum = __fadd_rn(sum, __fmul_rn(kernel.ptr(dy+half_size)[dx+half_size], src.ptr(y+dy)[x+dx]));
+				}
+			}
+			dst.ptr(y)[x] = clipGpu(sum);
+		}
+	}
 }
 
 void launch_convolution2dGpu
 (
-    const cv::cuda::GpuMat& src,
-    cv::cuda::GpuMat& dst,
-    const cv::cuda::GpuMat& kernel
+	const cv::cuda::GpuMat& src,
+	cv::cuda::GpuMat& dst,
+	const cv::cuda::GpuMat& kernel
 )
 {
-    const dim3 block(64, 2);
-    const dim3 grid(cv::cudev::divUp(dst.cols, block.x), cv::cudev::divUp(dst.rows, block.y));
+	const dim3 block(64, 2);
+	const dim3 grid(cv::cudev::divUp(dst.cols, block.x), cv::cudev::divUp(dst.rows, block.y));
 
-    convolution2dGpu<<<grid, block>>>(src, dst, kernel);
+	convolution2dGpu<<<grid, block>>>(src, dst, kernel);
 
-    CV_CUDEV_SAFE_CALL(cudaGetLastError());
-    CV_CUDEV_SAFE_CALL(cudaDeviceSynchronize());
+	CV_CUDEV_SAFE_CALL(cudaGetLastError());
+	CV_CUDEV_SAFE_CALL(cudaDeviceSynchronize());
 }
 
 double launch_convolution2dGpu
 (
-    const cv::cuda::GpuMat& src,
-    cv::cuda::GpuMat& dst,
-    const cv::cuda::GpuMat& kernel, 
-    const int loop_num
+	const cv::cuda::GpuMat& src,
+	cv::cuda::GpuMat& dst,
+	const cv::cuda::GpuMat& kernel, 
+	const int loop_num
 )
 {
-    double f = 1000.0f / cv::getTickFrequency();
-    int64 start = 0, end = 0;
-    double time = 0.0;
-    for (int i = 0; i <= loop_num; i++){
-        start = cv::getTickCount();
-        launch_convolution2dGpu(src, dst, kernel);
-        end = cv::getTickCount();
-        time += (i > 0) ? ((end - start) * f) : 0;
-    }
-    time /= loop_num;
+	double f = 1000.0f / cv::getTickFrequency();
+	int64 start = 0, end = 0;
+	double time = 0.0;
+	for (int i = 0; i <= loop_num; i++){
+		start = cv::getTickCount();
+		launch_convolution2dGpu(src, dst, kernel);
+		end = cv::getTickCount();
+		time += (i > 0) ? ((end - start) * f) : 0;
+	}
+	time /= loop_num;
 
-    return time;
+	return time;
 }
